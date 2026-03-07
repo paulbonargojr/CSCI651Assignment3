d7cb5606aef8085db09f0de2f6a9361b2eb9a22b
paulbonargo <paulbonjr@gmail.com>
2026-03-07 16:00:40 -0500

    Added basic CLI args, added function stubs for compute checksum and receive packet

--- reliable_data_transfer.py
+++ reliable_data_transfer.py
@@ -1,6 +1,5 @@
 import argparse
 import logging
-import socket
 
 # configuring warning output
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
@@ -12,6 +11,7 @@ sc.conf.iface = sc.get_working_if()
 
 VERBOSE = False
 PACKET_TIMEOUT = 4
+
 ACK_TYPE = 1
 NACK_TYPE = 0
 
@@ -24,6 +24,12 @@ def send_packet(data, host, port):
         
     return answered
 
+
+def receive_packet(packet):
+    # decompose packet to see if it is data or ACK, get seq num, get checksum and compute 
+    return False
+
+
 def send_ACK(ack_type, seq_num, checksum):
     
     if ack_type == ACK_TYPE:
@@ -33,3 +39,44 @@ def send_ACK(ack_type, seq_num, checksum):
     elif ack_type == NACK_TYPE:
         # send NACK with seq num and checksum
         print(f"NACK:\t{seq_num}") # revise
+        
+
+def compute_checksum(packet):
+    checksum = decompose(packet) # decompose packet to get checksum
+    
+    if checksum == len(packet): # see if checksum is len of packet
+        return True
+    
+    return False
+
+
+
+if __name__ == "__main__":
+
+    # ingest CLI call args TODO : add description and additional arguments
+    parser = argparse.ArgumentParser(description="")
+
+    # Address (positional argument) : Specify address to send packets to
+    parser.add_argument("address", 
+                        type=str,
+                        help="Address to send packet")
+    
+    # Port (positional argument) : Specify port to send packets through
+    parser.add_argument("port", 
+                        type=str,
+                        help="Port to send packet through")
+    
+    # timeout (-t) : Specify a timeout in seconds before ping exits regardless of how many packets have been received.
+    # None as default
+    parser.add_argument("-t",
+                        type=int,
+                        required=False,
+                        help="Seconds until timeout before data transfer terminates"
+                        ) # timeout in seconds
+
+    args = parser.parse_args()
+
+    target_ip = args.address
+    target_port = args.port
+    timeout_seconds = args.t
+    
