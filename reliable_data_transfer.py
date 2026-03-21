5f941d530afc59cde5e9ead58d553b484a7bf2ec
paulbonargo <paulbonjr@gmail.com>
2026-03-21 15:46:15 -0400

    refining build, send, receive packet and ACK

--- reliable_data_transfer.py
+++ reliable_data_transfer.py
@@ -21,19 +21,20 @@ NACK_TYPE = 0b0
 class RDTP(sc.Packet):
     name="Reliable Data Transfer Protocol"
     fields_desc = [
-        sc.Intfield("seq_num",0x00000000),
-        sc.IntField("ack_num",0x00000000),
-        sc.BitField("syn",0b0),
-        sc.BitField("ack",0b0),
-        sc.BitField("fin",0b0),
-        sc.IntField("checksum",0x00000000)
+        sc.Intfield("seq_num",0),
+        sc.IntField("ack_num",0),
+        sc.BitField("syn",0b0,1),
+        sc.BitField("ack",0b0,1),
+        sc.BitField("fin",0b0,1),
+        sc.IntField("padding",0, 5),
+        sc.IntField("checksum",0)
     ]
     
 # sc.IntField("src_port",0x00000000),
 # sc.IntField("dst_port",0x00000000),
 
+# sport = ""
 dport = ""
-sport = ""
 dst_address = "127.0.0.1"
 
 data = "HELLO"
@@ -41,8 +42,44 @@ data = "HELLO"
 packet = sc.IP(dst=dst_address) / RDTP(seq_num=-1,) / sc.UDP(dport=dport) / data
 
 
-def send_packet(data, host, port):
-            
+def compute_checksum(data: bytes):
+    """compute the checksum of data
+
+    Args:
+        data (bytes): data of packet to check sum of
+
+    Returns:
+        int: checksum value
+    """
+    return sum(data) % (2 ** 32)
+
+
+def validate_checksum(packet: sc.packet.Packet):
+    rdtp = packet[RDTP]
+    
+    packet_checksum = rdtp.checksum # decompose packet to get checksum field
+    computed_checksum = compute_checksum(packet) # compute checksum
+    
+    return computed_checksum == packet_checksum
+
+
+def build_packet(data, host, port, seq_num, ack_num, syn=False, ack=False, fin=False):
+    
+    header = RDTP(seq_num=seq_num, ack_num=ack_num, syn=syn, ack=ack, fin=fin, padding=0, checksum=0)
+    raw = bytes(header) + data
+    checksum = compute_checksum(raw)
+
+    header.checksum = checksum
+    # TODO : FIX THIS v
+    header.padding = -1
+    
+    return sc.IP(dst=host) / sc.UDP(dport=port) / header / data
+
+
+def send_packet(seq_num, data, host, port):
+    
+    pkt = build_packet(data, host, port, seq_num, 0)
+    sc.send(pkt, verbose=False)
     # TODO : v replace or wrap new header with ACK and SEQ NUM CHECKSUM data
     
     packet = sc.IP(dst=host) / sc.UDP(dport=port) / data
@@ -52,50 +89,57 @@ def send_packet(data, host, port):
     return answered
 
 
-def build_header(data, host, port, syn=False, ack=False):
+def send_ACK(packet_to_reply_to: sc.packet.Packet, seq_num):
+    
+    if not packet_to_reply_to.haslayer(sc.IP) or not packet_to_reply_to.haslayer(sc.UDP):
+        return None
+    
+    # send ACK with seq num and checksum
+    print(f"ACK:\t{seq_num}")
+        
+    # build packet+send
+    dst_address = packet_to_reply_to[sc.IP].src # get src address
+    dst_port = packet_to_reply_to[sc.UDP].sport # get src port number
+    
+    rdtp = RDTP(seq_num=0, ack_num=seq_num, ack=1, checksum=0)
+    
+    ack_packet = sc.IP(dst=dst_address) / sc.UDP(dport=dst_port) / rdtp
 
-    return     
+    data = bytes(ack_packet[RDTP])
+    ack_packet[RDTP].checksum = compute_checksum(data)
+
+    sc.send(ack_packet, verbose=False)
 
 
-def receive_packet(packet):
+
+def receive_packet(packet, expected_seq_num, received_data) -> tuple[int, bin]:
     # decompose packet to see if it is data or ACK, get seq num, get checksum and compute 
-    return False
 
+    if not packet.haslayer(RDTP):
+        return expected_seq_num, 0
 
-def send_ACK(packet_to_reply_to: sc.packet.Packet, ack_type, seq_num):
-    
-    if not packet_to_reply_to.haslayer(sc.IP):
-        return None
-    
-    if not packet_to_reply_to.haslayer(sc.UDP):
-        return None
-    
-    dst_address = packet_to_reply_to[sc.IP] # get src address
-    dst_port = packet_to_reply_to[sc.UDP] # get src port number
-    
-    if ack_type == ACK_TYPE:
-        # send ACK with seq num and checksum
-        print(f"ACK:\t{seq_num}") # revise
+    rdtp = packet[RDTP]
+
+    # check checksum
+    if not validate_checksum(packet):
+        print(f"[DROP]\t\tseq={rdtp.seq_num}")
+        return expected_seq_num, 0
+
+    # send ACK
+    if rdtp.seq == expected_seq_num:
+        print(f"[RECEIVED]\tseq_num={rdtp.seq_num}")
         
-        # build packet+send
+        received_data[rdtp.seq_num] = bytes(rdtp.payload)
         
-    elif ack_type == NACK_TYPE:
-        # send NACK with seq num and checksum
-        print(f"NACK:\t{seq_num}") # revise
-        
-        # build packet+send
+        send_ACK(rdtp.seq_num, packet)
+        expected_seq_num += 1
 
-
-def validate_checksum(packet: sc.packet.Packet):
+    # resend last ACK
+    else:
+        print(f"[OUT OF ORDER]: \n\texpected seq_num = {expected_seq_num}\n\treceived seq_num = {rdtp.seq_num}")
+        send_ACK(seq_num=expected_seq_num-1, packet_to_reply_to=packet)  
     
-    packet_checksum = packet.fields["checksum"] # decompose packet to get checksum
-    computed_checksum = sc.checksum(packet) # compute checksum
-    
-    # verify checksum value is equivalent to sum of packets bits 
-    if packet_checksum == computed_checksum: 
-        return True
-    
-    return False
+    return expected_seq_num, 1
 
 
 
